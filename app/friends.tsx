7910b4928a57d23ccbf816a16ad9aeb74abbf2ae
mgoodhue <mgoodhue22@gmail.com>
2024-11-10 16:00:42 -0500

    Added rectangle to top of friends page

--- app/friends.tsx
+++ app/friends.tsx
@@ -1,9 +1,16 @@
 import { Text, View, StyleSheet } from 'react-native';
 
+const Rectangle = () => {
+  return (
+    <View style={styles.rectangle} />
+  );
+};
+
 export default function FriendsScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Friends screen</Text>
+      <Rectangle/>
     </View>
   );
 }
@@ -14,6 +21,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#25292e',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  rectangle: {
+    width: '100%',
+    height: '18%',
+    backgroundColor: '#00487C',
+    position: 'absolute',
+    top: 0,
+    left: 0
   },
   text: {
     color: '#fff',
