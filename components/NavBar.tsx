1e050df2d84a87f37830eb8d82506eef6a07f1e1
pon-et <145276703+pon-et@users.noreply.github.com>
2024-11-10 16:37:38 -0500

    updated navbar
    used outline/filled icons and changing colors to show which page you are currently on

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,22 +1,67 @@
-import React from 'react';
-import { Link } from 'expo-router';
-import { View, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { useRouter, useSegments } from 'expo-router';
+import { View, StyleSheet, Pressable } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
 
 export default function NavBar() {
+    const [activeIcon, setActiveIcon] = useState("");
+    const router = useRouter();
+    const segments = useSegments();
+
+    useEffect(() => {
+        const path = segments[0];
+        if (path === 'map') {
+            setActiveIcon('map');
+        } else if (path === 'friends') {
+            setActiveIcon('friends');
+        } else if (path === 'profile') {
+            setActiveIcon('profile');
+        } else {
+            setActiveIcon("");
+        }
+    }, [segments]);
+
     return (
         <View style={styles.container}>
+            <Pressable 
+                onPress={() => {
+                    router.push('/map');
+                }} 
+                style={styles.button}
+            >
+                <Ionicons
+                    name={activeIcon === "map" ? "navigate" : "navigate-outline"}
+                    size={28}
+                    color={activeIcon === 'map' ? "#fff" : "#4BB3FD"}
+                />
+            </Pressable>
 
-          <Link href={{pathname: "./map"}}style= {styles.button}>
-          <Ionicons name="navigate" size={28} color="#fff" />
-          </Link>
-          <Link href={{pathname: "./friends"}}style= {styles.button}>
-          <Ionicons name="people" size={28} color="#fff" />
-          </Link>
-          <Link href={{pathname: "./profile"}}style= {styles.button}>
-          <Ionicons name="person-circle" size={28} color="#fff" />
-          </Link>
+            <Pressable 
+                onPress={() => {
+                    router.push('/friends');
+                }} 
+                style={styles.button}
+            >
+                <Ionicons
+                    name={activeIcon === 'friends' ? "people" : "people-outline"}
+                    size={28}
+                    color={activeIcon === 'friends' ? "#fff" : "#4BB3FD"}
+                />
+            </Pressable>
+          
+            <Pressable 
+                onPress={() => {
+                    router.push('/profile');
+                }} 
+                style={styles.button}
+            >
+                <Ionicons
+                    name={activeIcon === 'profile' ? "person-circle" : "person-circle-outline"}
+                    size={28}
+                    color={activeIcon === 'profile' ? "#fff" : "#4BB3FD"}
+                />
+            </Pressable>
         </View>
       );
 }
@@ -27,11 +72,10 @@ const styles = StyleSheet.create({
         bottom: 0,                 
         width: '100%',       
         height: 60,        
-        backgroundColor: '#32CD32',
+        backgroundColor: '#00487C',
         flexDirection: 'row',      
         justifyContent: 'space-around', 
         alignItems: 'center'
-        //paddingVertical: 10,  
     },
     button: {
         alignItems: 'center',  
