eab9b520f25bb3b54aba566937ccb91784c8c9b9
Lily-L-Lane <144285700+Lily-L-Lane@users.noreply.github.com>
2024-11-10 16:42:38 -0500

    map creation

--- app/map.tsx
+++ app/map.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet, TextInput, Button } from 'react-native';
 import { Link } from 'expo-router';
+import {APIProvider, Map} from '@vis.gl/react-google-maps';
 
 export default function MapScreen() {  
   return (
@@ -14,7 +15,17 @@ export default function MapScreen() {
 }
 
 //console.log(getNameTest);
-
+const MapApp = () => (
+  <APIProvider apiKey={'AIzaSyCucj_W_JnPNuhOGtDQQv_SAxd3IUWsz-c'}>
+    <Map
+      style={{width: '100vw', height: '100vh'}}
+      defaultCenter={{lat: 22.54992, lng: 0}}
+      defaultZoom={3}
+      gestureHandling={'greedy'}
+      disableDefaultUI={true}
+    />
+  </APIProvider>
+);
 
 const styles = StyleSheet.create({
   container: {
